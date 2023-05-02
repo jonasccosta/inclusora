a923a17bbeb176b61721f78f19c88fc74a33b385
Jonas Costa <38665874+jonasccosta@users.noreply.github.com>
2023-05-02 13:07:45 -0500

    Started basic UI

--- inclusora/src/app/app.module.ts
+++ inclusora/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  , TitleBarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
